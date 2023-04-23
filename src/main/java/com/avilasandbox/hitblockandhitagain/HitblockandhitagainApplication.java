ba7940972a42e5e69f51256a25398e529b36b82d
faku123faku <faku1234faku@gmail.com>
2023-04-23 11:49:05 -0300

    First commit

---
+++ src/main/java/com/avilasandbox/hitblockandhitagain/HitblockandhitagainApplication.java
@@ -0,0 +1,13 @@
+package com.avilasandbox.hitblockandhitagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitblockandhitagainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitblockandhitagainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avilasandbox/hitblockandhitagain/HitblockandhitagainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avilasandbox.hitblockandhitagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitblockandhitagainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
